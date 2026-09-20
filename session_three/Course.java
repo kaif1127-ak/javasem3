e0ed6cc64959020c7cfaf7de680543d197826d13
kaif1127-ak <kaifabdul11@gmail.com>
2026-09-20 16:12:55 +0530

    Add Session 3 practice programs

---
+++ session_three/Course.java
@@ -0,0 +1,35 @@
+class CourseDetails {
+    String code;
+    String title;
+    int credits;
+    int labCredits;
+
+    public CourseDetails(String code, String title, int credits, int labCredits) {
+        this.code = code;
+        this.title = title;
+        this.credits = credits;
+        this.labCredits = labCredits;
+    }
+
+    public CourseDetails(String code, String title, int credits) {
+        this(code, title, credits, 0);
+    }
+
+    public int totalCredits() {
+        return credits + labCredits;
+    }
+}
+
+public class Course {
+    public static void main(String[] args) {
+
+        CourseDetails theory =
+            new CourseDetails("21CSC201J", "Data Structures", 4);
+
+        CourseDetails lab =
+            new CourseDetails("21CSC205L", "DSA Lab", 3, 1);
+
+        System.out.println(theory.code + " total credits: " + theory.totalCredits());
+        System.out.println(lab.code + " total credits: " + lab.totalCredits());
+    }
+}
