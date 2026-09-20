7da53e2c2cba24286c965cce1df420cfd8914ceb
kaif1127-ak <kaifabdul11@gmail.com>
2026-09-20 12:56:50 +0530

    Add Session 2 homework programs

---
+++ session_two/StopWordFilteredWordFrequency.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class StopWordFilteredWordFrequency {
+
+    static void printFilteredWordFrequency(String feedback) {
+
+        String cleaned = feedback.toLowerCase()
+                .replace(".", "")
+                .replace(",", "");
+
+        String[] words = cleaned.split("\\s+");
+
+        String[] stopWords = {"the", "was", "and", "a", "is", "of", "in"};
+
+        HashMap<String, Integer> frequency = new HashMap<>();
+
+        for (String word : words) {
+
+            boolean isStopWord = false;
+
+            for (String stop : stopWords) {
+                if (word.equals(stop)) {
+                    isStopWord = true;
+                    break;
+                }
+            }
+
+            if (!isStopWord) {
+                frequency.put(word, frequency.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        ArrayList<Map.Entry<String, Integer>> list =
+                new ArrayList<>(frequency.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter feedback: ");
+        String feedback = sc.nextLine();
+
+        printFilteredWordFrequency(feedback);
+    }
+}
